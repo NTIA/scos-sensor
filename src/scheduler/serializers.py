9ce7fc4d878566c1bc07953baeb490c9f48780fb
Douglas Anderson <douglas.j.anderson@gmail.com>
2017-08-04 23:30:48 -0600

    make TaskSerializer use url convention of other serializers

--- src/scheduler/serializers.py
+++ src/scheduler/serializers.py
@@ -5,14 +5,12 @@ import actions
 
 
 class TaskSerializer(serializers.Serializer):
-    schedule_entry_name = serializers.SlugField()
-    schedule_entry_url = serializers.SerializerMethodField()
-    task_id = serializers.IntegerField(write_only=True)
+    schedule_entry = serializers.SerializerMethodField()
     action = serializers.CharField(max_length=actions.MAX_LENGTH)
     priority = serializers.IntegerField()
     time = serializers.IntegerField()
 
-    def get_schedule_entry_url(self, obj):
+    def get_schedule_entry(self, obj):
         request = self.context['request']
         return reverse('v1:schedule-detail',
                        args=(obj.schedule_entry_name,),
