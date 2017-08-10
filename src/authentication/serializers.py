fed5563472549b235ace7df05470e84f60275919
Douglas Anderson <danderson@ntia.doc.gov>
2017-08-10 17:17:15 -0600

    Expose read-only user details in API; new users are passwordless

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -4,29 +4,38 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     auth_token = serializers.SerializerMethodField()
+    has_usable_password = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = (
             'url',
             'username',
-            'password',
             'auth_token',
+            'has_usable_password',
+            'is_active',
+            'date_joined',
+            'last_login',
             'schedule_entries'
         )
         extra_kwargs = {
+            'is_active': {
+                'initial': True
+            },
             'schedule_entries': {
                 'view_name': 'v1:schedule-detail',
                 'lookup_field': 'name'
             },
-            'password': {
-                'style': {
-                    'input_type': 'password'
-                },
-                'write_only': True
-            }
         }
-        read_only_fields = ('auth_token', 'schedule_entries')
+        read_only_fields = (
+            'auth_token',
+            'schedule_entries',
+            'date_joined',
+            'last_login'
+        )
 
     def get_auth_token(self, obj):
         return obj.auth_token.key
+
+    def get_has_usable_password(self, obj):
+        return obj.has_usable_password()
