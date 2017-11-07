31e73af0a23898302f737919cdf7d68b87d801e9
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-07 13:42:50 -0700

    Clean up style.

--- src/acquisitions/tests/test_archive_view.py
+++ src/acquisitions/tests/test_archive_view.py
@@ -32,16 +32,12 @@ def test_archive_download(user_client, testclock):
         md = sigmf_archive_contents._metadata
         datafile = sigmf_archive_contents.data_file
         datafile_actual_size = os.stat(datafile).st_size
-
+        claimed_sha512 = md['global']['core:sha512']
         number_of_samples = len(md['annotations'])
         sample_count = md['annotations'][0]['core:sample_count']
         sample_size = sample_count * np.float64(0.0).nbytes
-
         datafile_expected_size = number_of_samples * sample_size
-
-        assert datafile_actual_size == datafile_expected_size
-
-        claimed_sha512 = md['global']['core:sha512']
         actual_sha512 = sigmf.sigmf_hash.calculate_sha512(datafile)
 
+        assert datafile_actual_size == datafile_expected_size
         assert claimed_sha512 == actual_sha512
