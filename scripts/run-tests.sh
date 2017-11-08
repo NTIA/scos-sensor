b947bb5bb3f790206d6aa3f4bbb09b86a884624c
Justin Haze <jhaze@ntia.doc.gov>
2017-11-08 12:27:37 -0700

    added docker files and scripts for docker test container; modified tox.ini for docker

---
+++ scripts/run-tests.sh
@@ -0,0 +1,17 @@
+
+
+#!/bin/bash
+
+set -e  # exit on error
+
+# You must run ./deploy.sh once before calling this script
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Stop running services
+echo "Bringing down running services"
+docker-compose -f ${REPO_ROOT}/docker-test/docker-compose.yml -p test_scossensor stop
+
+# Build out and run in background
+echo "Bringing up updated services"
+docker-compose -f ${REPO_ROOT}/docker-test/docker-compose.yml -p test_scossensor up -d --build
