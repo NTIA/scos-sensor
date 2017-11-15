789ec073e92f169ec563a5a151c57dc02fbeb686
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-15 16:37:31 -0700

    Add environmental variable `GUNICORN_LOG_LEVEL` to docker. Additionally, reflect this change in `entry_run.sh`.

--- scripts/entry_run.sh
+++ scripts/entry_run.sh
@@ -2,10 +2,10 @@
 
 set -e  # exit on error
 
-GUNICORN_LOG_LEVEL='info'
+# GUNICORN_LOG_LEVEL='info'
 
 echo 'Starting Migrations'
 python manage.py migrate
 
 echo 'Starting Gunicorn'
-gunicorn --bind :8000 sensor.wsgi --log-level $GUNICORN_LOG_LEVEL
+gunicorn --bind :8000 sensor.wsgi --log-level ${GUNICORN_LOG_LEVEL}
