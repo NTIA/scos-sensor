5decd03a068f9f0bdb32ddf8223e2fbeb69e1068
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-15 10:55:43 -0700

    Remove `makemigrations` call. Since we are committing our migration files, we only need to run `migrate`.

--- scripts/entry_run.sh
+++ scripts/entry_run.sh
@@ -5,7 +5,7 @@ set -e  # exit on error
 GUNICORN_LOG_LEVEL='info'
 
 echo 'Starting Migrations'
-python /src/manage.py makemigrations && /src/manage.py migrate
+python manage.py migrate
 
 echo 'Starting Gunicorn'
 gunicorn --bind :8000 sensor.wsgi --log-level $GUNICORN_LOG_LEVEL
