e5eae685a23bf4bd78d252bbdbe98923a049b80a
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-17 11:38:11 -0700

    Add a couple new tests to `test_views`. The first test needs to ensure that our new `admin_user_client` fixture works, as well as our new `TEST_PRIVATE_SCHEDULE_ENTRY` dict. The next test needs to ensure that a private `ScheduleEntry` created by an admin client cannot be seen by a layman user.

--- src/schedule/tests/test_views.py
+++ src/schedule/tests/test_views.py
@@ -2,7 +2,8 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 
 from acquisitions.tests.utils import simulate_acquisitions
-from schedule.tests import EMPTY_SCHEDULE_REPONSE, TEST_SCHEDULE_ENTRY
+from schedule.tests import (
+    EMPTY_SCHEDULE_REPONSE, TEST_SCHEDULE_ENTRY, TEST_PRIVATE_SCHEDULE_ENTRY)
 from schedule.tests.utils import post_schedule
 from sensor.tests.utils import validate_response
 
@@ -15,6 +16,25 @@ def test_post_schedule(user_client):
 
     for k, v in TEST_SCHEDULE_ENTRY.items():
         rjson[k] == v
+
+
+def test_post_admin_private_schedule(admin_user_client):
+    rjson = post_schedule(admin_user_client, TEST_PRIVATE_SCHEDULE_ENTRY)
+
+    for k, v in TEST_SCHEDULE_ENTRY.items():
+        rjson[k] == v
+
+
+def test_private_schedule_is_private(admin_user_client, user_client):
+    rjson = post_schedule(admin_user_client, TEST_PRIVATE_SCHEDULE_ENTRY)
+    entry_name = rjson['name']
+    entry_url = reverse('v1:schedule-detail', [entry_name])
+
+    user_respose = user_client.get(entry_url, **HTTPS_KWARG)
+    admin_user_respose = admin_user_client.get(entry_url, **HTTPS_KWARG)
+
+    validate_response(user_respose, status.HTTP_403_FORBIDDEN)
+    validate_response(admin_user_respose, status.HTTP_200_OK)
 
 
 def test_get_schedule(user_client):
