a0710df649dc18ab07a0699f8329afd610374b07
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-22 12:42:34 -0700

    Add view, modify, delete tests for laymen users. Test that a laymen user can view all non-private entries, but cannot view private entries.

--- src/schedule/tests/test_user_views.py
+++ src/schedule/tests/test_user_views.py
@@ -22,13 +22,75 @@ def test_user_cant_post_private_schedule(user_client):
     assert not user_respose.data['is_private']
 
 
-def test_user_cant_delete_admin_entry(admin_client, user_client):
+def test_user_can_view_non_private_entries(admin_client, user_client,
+                                           alternate_user_client):
+    # alternate user schedule entry
+    alternate_user_rjson = post_schedule(
+        alternate_user_client, TEST_SCHEDULE_ENTRY)
+    alternate_user_entry_name = alternate_user_rjson['name']
+    alternate_user_entry_url = reverse(
+        'v1:schedule-detail', [alternate_user_entry_name])
+
+    user_view_alternate_user_response = user_client.get(
+        alternate_user_entry_url, **HTTPS_KWARG)
+
+    # admin user schedule entry
+    admin_rjson = post_schedule(admin_client, TEST_ALTERNATE_SCHEDULE_ENTRY)
+    admin_entry_name = admin_rjson['name']
+    admin_entry_url = reverse('v1:schedule-detail', [admin_entry_name])
+
+    user_view_admin_response = user_client.get(admin_entry_url, **HTTPS_KWARG)
+
+    validate_response(
+        user_view_alternate_user_response, status.HTTP_200_OK)
+    validate_response(user_view_admin_response, status.HTTP_200_OK)
+
+
+def test_user_cant_view_private_entry(admin_client, user_client):
     rjson = post_schedule(admin_client, TEST_PRIVATE_SCHEDULE_ENTRY)
+    entry_name = rjson['name']
+    entry_url = reverse('v1:schedule-detail', [entry_name])
+
+    user_respose = user_client.get(entry_url, **HTTPS_KWARG)
+
+    validate_response(user_respose, status.HTTP_403_FORBIDDEN)
+
+
+def test_user_can_delete_their_entry(user_client):
+    rjson = post_schedule(user_client, TEST_SCHEDULE_ENTRY)
     entry_name = rjson['name']
     url = reverse('v1:schedule-detail', [entry_name])
 
     response = user_client.delete(url, **HTTPS_KWARG)
-    validate_response(response, status.HTTP_403_FORBIDDEN)
+    validate_response(response, status.HTTP_204_NO_CONTENT)
+
+    response = user_client.delete(url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_404_NOT_FOUND)
+
+
+def test_user_cant_delete_any_other_entry(admin_client, user_client,
+                                          alternate_user_client):
+    # alternate user schedule entry
+    alternate_user_rjson = post_schedule(
+        alternate_user_client, TEST_SCHEDULE_ENTRY)
+    alternate_user_entry_name = alternate_user_rjson['name']
+    alternate_user_entry_url = reverse(
+        'v1:schedule-detail', [alternate_user_entry_name])
+
+    user_delete_alternate_user_response = user_client.delete(
+        alternate_user_entry_url, **HTTPS_KWARG)
+
+    # admin user schedule entry
+    admin_rjson = post_schedule(admin_client, TEST_PRIVATE_SCHEDULE_ENTRY)
+    admin_entry_name = admin_rjson['name']
+    admin_entry_url = reverse('v1:schedule-detail', [admin_entry_name])
+
+    user_delete_admin_response = user_client.delete(
+        admin_entry_url, **HTTPS_KWARG)
+
+    validate_response(
+        user_delete_alternate_user_response, status.HTTP_403_FORBIDDEN)
+    validate_response(user_delete_admin_response, status.HTTP_403_FORBIDDEN)
 
 
 def test_user_can_modify_their_entry(user_client):
