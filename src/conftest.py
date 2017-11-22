d23340b1bf2b78360196f058d17bb8d549bfa416
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-22 11:39:07 -0700

    Add alternate user and admin clients.

--- src/conftest.py
+++ src/conftest.py
@@ -54,6 +54,69 @@ def user_client(db, user):
     return client
 
 
+@pytest.fixture
+def alternate_user(db):
+    """A normal user."""
+    username = 'alternate_test'
+    password = 'password'
+
+    user, created = User.objects.get_or_create(username=username)
+
+    if created:
+        user.set_password(password)
+        user.save()
+
+    user.password = password
+
+    return user
+
+
+@pytest.fixture
+def alternate_user_client(db, alternate_user):
+    """A Django test client logged in as a normal user"""
+    client = Client()
+    client.login(
+        username=alternate_user.username, password=alternate_user.password)
+
+    return client
+
+
+@pytest.fixture
+def alternate_admin_user(db, django_user_model, django_username_field):
+    """A Django admin user.
+    This uses an existing user with username "admin", or creates a new one with
+    password "password".
+    """
+    UserModel = django_user_model
+    username_field = django_username_field
+
+    try:
+        user = UserModel._default_manager.get(
+            **{username_field: 'alternate_admin'})
+    except UserModel.DoesNotExist:
+        extra_fields = {}
+
+        if username_field != 'username':
+            extra_fields[username_field] = 'alternate_admin'
+
+        user = UserModel._default_manager.create_superuser(
+            'alternate_admin', 'alternate_admin@example.com', 'password',
+            **extra_fields)
+
+    return user
+
+
+@pytest.fixture
+def alternate_admin_client(db, alternate_admin_user):
+    """A Django test client logged in as an admin user."""
+    from django.test.client import Client
+
+    client = Client()
+    client.login(username=alternate_admin_user.username, password='password')
+
+    return client
+
+
 # Add mock acquisitions for tests
 mock_acquire = actions.acquire_single_freq_fft.SingleFrequencyFftAcquisition(
     frequency=1e9,    # 1 GHz
