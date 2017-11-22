5309b47fe0d6a3771b7227277e650c94f0e236d0
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-22 10:30:52 -0700

    Test that admins can view any user's entry.

--- src/schedule/tests/test_admin_views.py
+++ src/schedule/tests/test_admin_views.py
@@ -23,21 +23,39 @@ def test_post_admin_private_schedule(admin_client):
     assert admin_user_respose.data['is_private'] is True
 
 
-def test_admin_can_delete_all_entries(admin_client, user_client):
-    # laymen user schedule entry
-    laymen_rjson = post_schedule(user_client, TEST_SCHEDULE_ENTRY)
-    laymen_entry_name = laymen_rjson['name']
-    laymen_url = reverse('v1:schedule-detail', [laymen_entry_name])
-
-    response = admin_client.delete(laymen_url, **HTTPS_KWARG)
-    validate_response(response, status.HTTP_204_NO_CONTENT)
-    response = admin_client.delete(laymen_url, **HTTPS_KWARG)
-    validate_response(response, status.HTTP_404_NOT_FOUND)
+def test_admin_can_view_all_entries(admin_client, user_client):
+    # user schedule entry
+    user_rjson = post_schedule(user_client, TEST_SCHEDULE_ENTRY)
+    user_entry_name = user_rjson['name']
+    user_url = reverse('v1:schedule-detail', [user_entry_name])
 
     # admin user schedule entry
     admin_rjson = post_schedule(admin_client, TEST_PRIVATE_SCHEDULE_ENTRY)
     admin_entry_name = admin_rjson['name']
     admin_url = reverse('v1:schedule-detail', [admin_entry_name])
+
+    response = admin_client.get(user_url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_200_OK)
+
+    response = admin_client.get(admin_url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_200_OK)
+
+
+def test_admin_can_delete_all_entries(admin_client, user_client):
+    # user schedule entry
+    user_rjson = post_schedule(user_client, TEST_SCHEDULE_ENTRY)
+    user_entry_name = user_rjson['name']
+    user_url = reverse('v1:schedule-detail', [user_entry_name])
+
+    # admin user schedule entry
+    admin_rjson = post_schedule(admin_client, TEST_PRIVATE_SCHEDULE_ENTRY)
+    admin_entry_name = admin_rjson['name']
+    admin_url = reverse('v1:schedule-detail', [admin_entry_name])
+
+    response = admin_client.delete(user_url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_204_NO_CONTENT)
+    response = admin_client.delete(user_url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_404_NOT_FOUND)
 
     response = admin_client.delete(admin_url, **HTTPS_KWARG)
     validate_response(response, status.HTTP_204_NO_CONTENT)
