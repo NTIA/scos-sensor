390397e51b3bf23a5d881083d3d024e05056701a
Dylan Hicks <dhicks@ntia.doc.gov>
2017-11-30 10:22:06 -0700

    Run `create_superuser.py` in the entry script.

--- docker/api_entrypoint.sh
+++ docker/api_entrypoint.sh
@@ -5,5 +5,8 @@ set -e  # exit on error
 echo 'Starting Migrations'
 python manage.py migrate
 
+echo 'Attempting to Create Superuser'
+python /scripts/create_superuser.py
+
 echo 'Starting Gunicorn'
 exec gunicorn --bind :8000 sensor.wsgi --log-level ${GUNICORN_LOG_LEVEL}
