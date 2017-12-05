3f635ee1d619516d6af1709e9703eac42b7a39a1
Justin Haze <jhaze@ntia.doc.gov>
2017-12-05 16:27:08 -0700

    Added calculation for equivalent noise bandwidth. Added value for reference and equivalent_noise_bandwidth in SingleFrequencyFFTDetection Object metadata.

--- src/actions/acquire_single_freq_fft.py
+++ src/actions/acquire_single_freq_fft.py
@@ -85,6 +85,7 @@ class SingleFrequencyFftAcquisition(Action):
         self.fft_size = fft_size
         self.nffts = nffts
         self.usrp = usrp  # make instance variable to allow hotswapping mock
+        self.enbw = None
 
     def __call__(self, schedule_entry_name, task_id):
         from schedule.models import ScheduleEntry
@@ -164,15 +165,14 @@ class SingleFrequencyFftAcquisition(Action):
             single_frequency_fft_md = {
                 "number_of_samples_in_fft": self.fft_size,
                 "window": "blackman",
-                "equivalent_noise_bandwidth": "",
-                "detector": detector.name,
+                "equivalent_noise_bandwidth": self.enbw,
+                "detector": detector.name + "_power",
                 "number_of_ffts": self.nffts,
                 "units": "dBm",
-                "reference": ""
+                "reference": "not referenced"
             }
 
             annotation_md = {
-                # "scos:detector": detector.name,
                 "scos:measurement_type": {
                         "SingleFrequencyFFTDetection": single_frequency_fft_md
                 }
@@ -191,8 +191,10 @@ class SingleFrequencyFftAcquisition(Action):
         logger.debug("Applying detector")
 
         window = np.blackman(self.fft_size)
-        window_power = sum(tap*tap for tap in window)
+        window_power = sum(window**2)
         impedance = 50.0  # ohms
+
+        self.enbw = self.fft_size * window_power / sum(window)**2
 
         Vsq2W_dB = -10.0 * np.log10(self.fft_size * window_power * impedance)
 
