d1ba99ba0ccc2689432b6f2d435ab17f625d066c
Justin Haze <jhaze@ntia.doc.gov>
2017-12-07 13:47:01 -0700

    Put both tests into test_detector since test_detector was subset of test_validate_sigmf_output

--- src/actions/tests/test_acquire_single_freq_fft.py
+++ src/actions/tests/test_acquire_single_freq_fft.py
@@ -9,7 +9,7 @@ from sigmf.validate import validate
 from .mocks import usrp as mock_usrp
 
 
-def run_single_frequency_fft_acquisition(user_client):
+def test_detector(user_client):
     # Put an entry in the schedule that we can refer to
     rjson = post_schedule(user_client, TEST_SCHEDULE_ENTRY)
     entry_name = rjson['name']
@@ -24,15 +24,6 @@ def run_single_frequency_fft_acquisition(user_client):
     )
     action.usrp = mock_usrp
     action(entry_name, task_id)
-    return task_id
-
-
-def test_detector(user_client):
-    run_single_frequency_fft_acquisition(user_client)
-
-
-def test_validate_sigmf_output(user_client):
-    task_id = run_single_frequency_fft_acquisition(user_client)
     acquistion = Acquisition.objects.get(task_id=task_id)
     sigmf_metadata = acquistion.sigmf_metadata
     assert validate(sigmf_metadata)
