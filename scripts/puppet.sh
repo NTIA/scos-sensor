2f3b1936464d33d6784d51f48d62f33e8ea5cbab
root <root@smsforeman01.sms.internal>
2017-12-14 15:49:28 -0700

    Adding script to create puppet module.

---
+++ scripts/puppet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Use this script to create the `scos` Puppet module on the Puppet Master which
+# will deploy the scos-sensor code to new sensors.
+
+# Pseudocode
+
+# determine where puppet is installed
+# get a list of puppet environments on puppet master  
+# user inputs which location to deploy to
+# create folder structure
+# copy manifest files
+# copy fact generation files
+# copy scos-code files 
+  
