0321274ee644f927b8a634c2418b8ca95fd4fb7a
Dylan Hicks <dhicks@ntia.doc.gov>
2017-12-15 13:15:25 -0700

    Start to allow any user to get api/vi/schema

---
+++ src/sensor/views.py
@@ -0,0 +1,45 @@
+from rest_framework import exceptions
+from rest_framework.permissions import AllowAny
+from rest_framework.renderers import CoreJSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.schemas import SchemaGenerator
+from rest_framework.views import APIView
+
+from .schema import OpenAPIRenderer
+
+
+def get_swagger_view(title=None, url=None, patterns=None, urlconf=None):
+    """
+    Returns schema view which renders Swagger/OpenAPI.
+    """
+    class SwaggerSchemaView(APIView):
+        _ignore_model_permissions = True
+        exclude_from_schema = True
+        # authentication_classes = []
+        permission_classes = [AllowAny]
+        renderer_classes = [
+            CoreJSONRenderer,
+            BrowsableAPIRenderer,
+            OpenAPIRenderer
+        ]
+
+        def get(self, request):
+            generator = SchemaGenerator(
+                title=title,
+                url=url,
+                patterns=patterns,
+                urlconf=urlconf
+            )
+
+            print(request)
+
+            schema = generator.get_schema(request=request)
+
+            if not schema:
+                raise exceptions.ValidationError(
+                    'The schema generator did not return a schema Document'
+                )
+
+            return Response(schema)
+
+    return SwaggerSchemaView.as_view()
