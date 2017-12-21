8b9597a9bf5f623165e8fc10ad135d83bd630b1f
Justin Haze <jhaze@ntia.doc.gov>
2017-12-21 12:15:54 -0700

    Added json schema and schema check for antenna, data_extraction_unit, and single_frequency_fft_detection_object

--- src/actions/tests/test_acquire_single_freq_fft.py
+++ src/actions/tests/test_acquire_single_freq_fft.py
@@ -2,10 +2,18 @@ from __future__ import absolute_import
 
 from actions import by_name
 from acquisitions.models import Acquisition
+from django.conf import settings
+from jsonschema import validate as schema_validate
 from schedule.tests import TEST_SCHEDULE_ENTRY
 from schedule.tests.utils import post_schedule
-from sigmf.validate import validate
+from sigmf.validate import validate as sigmf_validate
 
+import json
+import os
+
+SCHEMA_PATH = os.path.join(settings.REPO_ROOT, "config", "scos_transfer_spec_schema.json")
+with open(SCHEMA_PATH, "r") as f:
+    schema = json.load(f)
 
 def test_detector(user_client):
     # Put an entry in the schedule that we can refer to
@@ -17,4 +25,5 @@ def test_detector(user_client):
     by_name['mock_acquire'](entry_name, task_id)
     acquistion = Acquisition.objects.get(task_id=task_id)
     sigmf_metadata = acquistion.sigmf_metadata
-    assert validate(sigmf_metadata)
+    assert sigmf_validate(sigmf_metadata)
+    schema_validate(sigmf_metadata, schema)
