85040ab8da621c9e8c37e96b9e3c097c1940803b
Douglas Anderson <danderson@ntia.doc.gov>
2017-12-27 09:52:10 -0700

    Fix missed directory name change

--- jenkins/scos-sensor-docker.sh
+++ jenkins/scos-sensor-docker.sh
@@ -5,7 +5,7 @@ if [ -d "test_results" ]; then
     rm -r "test_results" &> rm_out.txt
 fi
 docker build -f Dockerfile -t testscossensor . &> docker_build_out.txt
-docker run --name testscossensor_1 -e SECRET_KEY -e DEBUG -e DOMAINS -e IPS -d testscossensor /docker/testing_entrypoint.sh &> docker_run_out.txt
+docker run --name testscossensor_1 -e SECRET_KEY -e DEBUG -e DOMAINS -e IPS -d testscossensor /entrypoints/testing_entrypoint.sh &> docker_run_out.txt
 docker logs -f testscossensor_1 &> docker_logs_out.txt
 docker cp testscossensor_1:/test_results . &> docker_cp_out.txt
 docker rm testscossensor_1 &> docker_rm_2_out.txt
