ee01b7034ada5cdb48146dbd379dd4ca35b61da8
Douglas Anderson <danderson@ntia.doc.gov>
2017-12-29 16:37:20 -0700

    Don't overrite REPO_ROOT if its previously set

--- scripts/build_api.sh
+++ scripts/build_api.sh
@@ -2,6 +2,6 @@
 
 set -e  # exit on error
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
+REPO_ROOT=${REPO_ROOT:=$(git rev-parse --show-toplevel)}
 
 docker build -f ${REPO_ROOT}/Dockerfile -t smsntia/scos-sensor ${REPO_ROOT}
