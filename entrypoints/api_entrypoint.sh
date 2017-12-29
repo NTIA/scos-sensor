6794fb7ede9031a0b0a4090c20c2da26ad3f8ef0
Douglas Anderson <danderson@ntia.doc.gov>
2017-12-29 13:00:01 -0700

    Fail early if user didn't `touch` db file and provide useful msg

--- entrypoints/api_entrypoint.sh
+++ entrypoints/api_entrypoint.sh
@@ -2,11 +2,22 @@
 
 set -e  # exit on error
 
-echo 'Starting Migrations'
+DB_PATH=/db.sqlite3
+
+if [ -d $DB_PATH ]; then
+    echo "The database file $DB_PATH didn't exist, so Docker mounted it as a directory."
+    echo "Use the following commands to fix the issue:"
+    echo "$ rm -r .${DB_PATH}/"
+    echo "$ touch .$DB_PATH"
+    echo "$ docker-compose rm"
+    exit 1
+fi
+
+echo "Starting Migrations"
 python manage.py migrate
 
-echo 'Creating Superuser'
+echo "Creating Superuser"
 python /scripts/create_superuser.py
 
-echo 'Starting Gunicorn'
+echo "Starting Gunicorn"
 exec gunicorn sensor.wsgi -c ../gunicorn/config.py
