dc25e09effbd87c5d03b384748f3aa83549879c9
Douglas Anderson <danderson@ntia.doc.gov>
2018-01-08 14:48:31 -0700

    Create openapi json file if it doesn't exist

--- src/sensor/tests/test_api_docs.py
+++ src/sensor/tests/test_api_docs.py
@@ -11,6 +11,6 @@ def test_api_docs_up_to_date(admin_client):
     """Ensure that docs/openapi.json is up-to-date."""
     schema_url = reverse('api_schema', kwargs=V1)
     response = admin_client.get(schema_url)
-    with open(settings.OPENAPI_FILE, 'w') as openapi_file:
+    with open(settings.OPENAPI_FILE, 'w+') as openapi_file:
         openapi_json = json.loads(response.content)
         json.dump(openapi_json, openapi_file, indent=4)
