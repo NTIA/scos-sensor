cad439812916950f569c2f0739a70a470354a27a
Douglas Anderson <danderson@ntia.doc.gov>
2018-01-19 17:04:38 -0700

    Specify version
    To be honest, I'm not sure why this was necessary

--- src/acquisitions/serializers.py
+++ src/acquisitions/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from schedule.models import ScheduleEntry
+from sensor import V1
 from .models import Acquisition
 
 
@@ -45,6 +46,7 @@ class AcquisitionHyperlinkedRelatedField(serializers.HyperlinkedRelatedField):
             'schedule_entry_name': obj.schedule_entry.name,
             'task_id': obj.task_id
         }
+        kws.update(V1)
         return reverse(view_name, kwargs=kws, request=request, format=format)
 
 
