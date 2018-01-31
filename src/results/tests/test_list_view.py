884448596e5b4782a2d7067aa15f984c27bf804e
Douglas Anderson <danderson@ntia.doc.gov>
2018-01-31 11:29:02 -0700

    Add tests for task result overview and list views

---
+++ src/results/tests/test_list_view.py
@@ -0,0 +1,51 @@
+import pytest
+from rest_framework import status
+
+from results.tests.utils import (
+    create_task_results,
+    get_result_list,
+    reverse_result_detail,
+    reverse_result_list,
+)
+from sensor.tests.utils import validate_response, HTTPS_KWARG
+
+
+def test_non_existent_entry(user_client):
+    with pytest.raises(AssertionError):
+        get_result_list(user_client, 'doesntexist')
+
+
+@pytest.mark.django_db
+def test_single_result_response(user_client):
+    entry_name = create_task_results(1, user_client)
+    result, = get_result_list(user_client, entry_name)
+    task_id = 1
+    expected_url = reverse_result_detail(entry_name, task_id)
+    assert result['url'] == expected_url
+    assert result['task_id'] == task_id
+
+
+@pytest.mark.django_db
+def test_multiple_result_response(user_client, test_scheduler):
+    entry_name = create_task_results(3, user_client)
+    results = get_result_list(user_client, entry_name)
+    assert len(results) == 3
+
+    for i, acq in enumerate(results, start=1):
+        expected_url = reverse_result_detail(entry_name, i)
+        assert acq['url'] == expected_url
+        assert acq['task_id'] == i
+
+
+@pytest.mark.django_db
+def test_delete_list(user_client):
+    # If result doesn't exist, expect 405
+    url = reverse_result_list('doesntexist')
+    response = user_client.delete(url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # If result does exist, expect 405
+    entry_name = create_task_results(1, user_client)
+    url = reverse_result_list(entry_name)
+    response = user_client.delete(url, **HTTPS_KWARG)
+    validate_response(response, status.HTTP_405_METHOD_NOT_ALLOWED)
---
+++ src/results/tests/test_overview_view.py
@@ -0,0 +1,28 @@
+from rest_framework import status
+
+from results.tests.utils import (
+    EMPTY_RESULTS_RESPONSE,
+    create_task_results,
+    reverse_results_overview,
+    get_results_overview
+)
+from sensor.tests.utils import validate_response, HTTPS_KWARG
+
+
+def test_empty_overview_response(user_client):
+    response = get_results_overview(user_client)
+    assert response == EMPTY_RESULTS_RESPONSE
+
+
+def test_get_overview(user_client):
+    create_task_results(2, user_client)
+    overview, = get_results_overview(user_client)
+    assert overview['results_available'] == 2
+    assert overview['url']  # is non-empty string
+    assert overview['schedule_entry']  # is non-empty string
+
+
+def test_delete_overview_not_allowed(user_client):
+    url = reverse_results_overview()
+    response = user_client.delete(url, **HTTPS_KWARG)
+    assert validate_response(response, status.HTTP_405_METHOD_NOT_ALLOWED)
