7e7bb1539430c38c2012d66551999ff224ab37de
Douglas Anderson <danderson@ntia.doc.gov>
2018-02-08 13:39:26 -0700

    Use choices for 'result' field of TaskResult

--- src/results/models.py
+++ src/results/models.py
@@ -7,6 +7,12 @@ from .consts import MAX_DETAIL_LEN
 
 class TaskResult(models.Model):
     """Map between schedule entries and their task results."""
+    SUCCESS = 1
+    FAILURE = 2
+    RESULT_CHOICES = (
+        (SUCCESS, 'success'),
+        (FAILURE, 'failure')
+    )
     schedule_entry = models.ForeignKey(
         ScheduleEntry,
         related_name='results',
@@ -18,7 +24,8 @@ class TaskResult(models.Model):
     started = models.DateTimeField(help_text="The time the task started")
     finished = models.DateTimeField(help_text="The time the task finished")
     duration = models.DurationField(help_text="Task duration in seconds")
-    result = models.CharField(max_length=7, help_text='"success" or "failure"')
+    result = models.CharField(max_length=7, help_text='"success" or "failure"',
+                              choices=RESULT_CHOICES)
     detail = models.CharField(
         max_length=MAX_DETAIL_LEN,
         blank=True,
