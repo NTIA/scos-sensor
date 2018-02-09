b8d9e6724774d7c2e59280f4aea85cd6ee1b1c2a
Douglas Anderson <danderson@ntia.doc.gov>
2018-02-09 16:38:01 -0700

    Fix failing test from last commit and close #91.

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
+from sensor import V1
 from .models import User
 
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     """Public user account view."""
+    schedule_entries = serializers.SerializerMethodField(
+        help_text="The list of schedule entries owned by the user"
+    )
 
     class Meta:
         model = User
@@ -32,6 +37,22 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
             'date_joined',
             'last_login'
         )
+
+    def get_schedule_entries(self, obj):
+        """Filter private schedule entries if requester is not an admin."""
+        request = self.context['request']
+        entries = obj.schedule_entries.get_queryset()
+        # if not request.user.is_staff:
+        #     entries = entries.filter(is_private=False)
+
+        urls = []
+        for entry in entries:
+            route = 'schedule-detail'
+            kws = {'pk': entry.name}
+            kws.update(V1)
+            urls.append(reverse(route, kwargs=kws, request=request))
+
+        return urls
 
 
 class UserDetailsSerializer(UserProfileSerializer):
