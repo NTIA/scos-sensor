e744fa53be4f8aa6c15e587d1c821a86ab183919
Douglas Anderson <danderson@ntia.doc.gov>
2018-03-12 17:34:34 -0600

    Make restore_capabilities script run without local python dev env

--- scripts/restore_capabilities.sh
+++ scripts/restore_capabilities.sh
@@ -28,20 +28,34 @@ if [[ ! "$INPUT" || "$INPUT" == "-h" || "$INPUT" == "--help" ]]; then
     exit 0
 fi
 
-if [[ ! -e "$INPUT" ]]; then
-    echo "Fixture file \"$INPUT\" doesn't exist."
+HOST_PATH=$(readlink -e "$INPUT")
+FILENAME=$(basename "$INPUT")
+CONTAINER_PATH="/tmp/$FILENAME"
+
+if [[ ! -e "$HOST_PATH" ]]; then
+    echo "Fixture file \"$HOST_PATH\" doesn't exist."
     exit 1
 fi
 
 set +e  # this command may "fail"
 DB_RUNNING=$(docker-compose -f ${REPO_ROOT}/docker-compose.yml ps db |grep Up)
+API_RUNNING=$(docker-compose -f ${REPO_ROOT}/docker-compose.yml ps api |grep Up)
 set -e
 
 # Ensure database container is running
 docker-compose -f ${REPO_ROOT}/docker-compose.yml up -d db
 
 # Load given fixture file into database
-python ${REPO_ROOT}/src/manage.py loaddata "$INPUT"
+if [[ "$API_RUNNING" ]]; then
+    API_CONTAINER=$(docker-compose -f ${REPO_ROOT}/docker-compose.yml ps -q api)
+    docker cp "$HOST_PATH" scossensor_api_1:/tmp
+    docker-compose -f ${REPO_ROOT}/docker-compose.yml exec api \
+                   /src/manage.py loaddata "$CONTAINER_PATH"
+else
+    docker-compose  -f ${REPO_ROOT}/docker-compose.yml run \
+                    -v "$HOST_PATH":"$CONTAINER_PATH" \
+                    --rm api /src/manage.py loaddata "$CONTAINER_PATH"
+fi
 
 # If the DB was already running, leave it up
 if [[ ! "$DB_RUNNING" ]]; then
