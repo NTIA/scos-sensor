296160670a316a2d288f238d124133d3bab9d6dc
Douglas Anderson <danderson@ntia.doc.gov>
2018-11-07 20:03:49 -0700

    Add a script that prints out a parameterized description string

---
+++ scripts/print_action_docstring.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, print_function
+
+import os
+import sys
+import django
+
+PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'src')
+
+sys.path.append(PATH)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensor.settings")
+django.setup()
+
+from actions import registered_actions  # noqa
+
+action_names = registered_actions.keys()
+action_names.sort()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=action_names,
+                        help="Name of action to get docstring for"),
+
+    args = parser.parse_args()
+    print(registered_actions[args.action].description)
