3bb98af5508998ad37f5ee1b90d45d45632207aa
Justin Haze <jhaze@ntia.doc.gov>
2018-11-28 08:40:40 -0700

    Renamed MOCK to MOCK_RADIO, pass as unset environment variable

--- src/hardware/usrp_iface.py
+++ src/hardware/usrp_iface.py
@@ -37,7 +37,7 @@ def connect(sf_file=settings.SCALE_FACTORS_FILE):  # -> bool:
     global is_available
     global radio
 
-    if settings.RUNNING_DEMO or settings.RUNNING_TESTS or settings.MOCK:
+    if settings.RUNNING_DEMO or settings.RUNNING_TESTS or settings.MOCK_RADIO:
         logger.warning("Using mock USRP.")
 
         usrp = MockUsrpBlock()
