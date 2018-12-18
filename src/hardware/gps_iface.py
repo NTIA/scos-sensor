4951fd2a63b27c0847714caee2b992e8b745c78a
Brad Eales <bradeales@users.noreply.github.com>
2018-12-18 11:59:24 -0700

    Output the raw NMEA string.

--- src/hardware/gps_iface.py
+++ src/hardware/gps_iface.py
@@ -86,7 +86,9 @@ def get_lat_long(timeout_s=1):
         gpgga = usrp.get_mboard_sensor('gps_gpgga').value
         (fmt, utc, lat, ns, lng, ew, qual, nsats, hdil, alt, altu, gdalsep,
          gdalsepu, age, refid) = gpgga.split(',')
-
+        
+        print("GPS NMEA: {}".format(gpgga))
+        
         latitude = float(lat)
         if ns == 'S':
             latitude = -latitude
