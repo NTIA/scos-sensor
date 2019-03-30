8eb7952efb6e47b2e1ecfa3408afeaf7b84544f2
Douglas Anderson <douglas.j.anderson@gmail.com>
2019-03-30 00:26:25 -0600

    Remove SensorDefinition object from database

--- src/capabilities/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .antenna import Antenna  # noqa
-from .receiver import Receiver  # noqa
-from .rfpath import RFPath  # noqa
-from .preselector import Preselector  # noqa
-from .sensor_definition import SensorDefinition  # noqa
