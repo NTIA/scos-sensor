4262b649df2950b7d8207ab670d309426539611b
Douglas Anderson <douglas.j.anderson@gmail.com>
2019-05-07 23:13:05 -0600

    Refactor action yaml loader test

--- src/actions/tests/test_init.py
+++ src/actions/tests/test_init.py
@@ -39,40 +39,28 @@ def test_load_from_yaml_existing():
     actions.load_from_yaml()
 
 
-def test_load_from_yaml_parse_error():
-    """An invalid yaml file should cause a parse error."""
+def _test_load_from_yaml_check_error(yaml_to_write, expected_error):
     # load_from_yaml loads all `.yml` files in the passed directory, so do a
     # bit of setup to create an invalid yaml tempfile in a temporary directory
     with tempfile.TemporaryDirectory() as tmpdir:
         with tempfile.NamedTemporaryFile(suffix='.yml', dir=tmpdir) as tmpfile:
-            tmpfile.write(INVALID_YAML)
+            tmpfile.write(yaml_to_write)
             tmpfile.seek(0)
             # Now try to load the invalid yaml file, expecting an error
-            with pytest.raises(ScannerError):
+            with pytest.raises(expected_error):
                 actions.load_from_yaml(yaml_dir=tmpdir)
+
+
+def test_load_from_yaml_parse_error():
+    """An invalid yaml file should cause a parse error."""
+    _test_load_from_yaml_check_error(INVALID_YAML, ScannerError)
 
 
 def test_load_from_yaml_invalid_class_name():
     """A nonexistent action class name should raise an error."""
-    # load_from_yaml loads all `.yml` files in the passed directory, so do a
-    # bit of setup to create an invalid yaml tempfile in a temporary directory
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with tempfile.NamedTemporaryFile(suffix='.yml', dir=tmpdir) as tmpfile:
-            tmpfile.write(NONEXISTENT_ACTION_CLASS_NAME)
-            tmpfile.seek(0)
-            # Now try to load the invalid yaml file, expecting an error
-            with pytest.raises(KeyError):
-                actions.load_from_yaml(yaml_dir=tmpdir)
+    _test_load_from_yaml_check_error(NONEXISTENT_ACTION_CLASS_NAME, KeyError)
 
 
 def test_load_from_yaml_invalid_parameters():
     """A nonexistent action class name should raise an error."""
-    # load_from_yaml loads all `.yml` files in the passed directory, so do a
-    # bit of setup to create an invalid yaml tempfile in a temporary directory
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with tempfile.NamedTemporaryFile(suffix='.yml', dir=tmpdir) as tmpfile:
-            tmpfile.write(INVALID_PARAMETERS)
-            tmpfile.seek(0)
-            # Now try to load the invalid yaml file, expecting an error
-            with pytest.raises(TypeError):
-                actions.load_from_yaml(yaml_dir=tmpdir)
+    _test_load_from_yaml_check_error(INVALID_PARAMETERS, TypeError)
