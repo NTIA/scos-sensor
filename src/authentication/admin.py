1a664cb29513d1ceba47eead45093155ed91667a
Douglas Anderson <danderson@ntia.doc.gov>
2019-05-15 14:30:36 -0600

    Remove trace of unused server_url User property

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -6,14 +6,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-class SensorUserAdmin(UserAdmin):
-    model = User
-
-    fieldsets = UserAdmin.fieldsets + (
-        # server_url unforunately gets placed at bottom of User admin page
-        (None, {
-            'fields': ('server_url', )
-        }), )
-
-
-admin.site.register(User, SensorUserAdmin)
+admin.site.register(User, UserAdmin)
