3854472bb0b79d80aafc0940e49ad0939686abdf
Douglas Anderson <douglas.j.anderson@gmail.com>
2019-05-31 00:45:58 -0600

    Make isort smarter

--- scripts/autoformat_python.sh
+++ scripts/autoformat_python.sh
@@ -3,9 +3,11 @@
 # Autoformat python - sort imports and then "blacken" code
 
 REPO_ROOT=${REPO_ROOT:=$(git rev-parse --show-toplevel)}
+SRC_ROOT=${REPO_ROOT}/src
 
 echo "Sorting imports with isort... "
-isort -rc ${REPO_ROOT}/src/
+seed-isort-config --application-directories=${SRC_ROOT} --settings-path=${SRC_ROOT}
+isort -rc ${SRC_ROOT}
 echo
 echo "Formatting code with black... "
-black ${REPO_ROOT}/src/
+black ${SRC_ROOT}
