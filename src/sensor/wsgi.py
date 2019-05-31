7be9a001302b194f4406f290d2f94cf057959e34
Douglas Anderson <douglas.j.anderson@gmail.com>
2019-05-31 01:08:57 -0600

    Disable isort in wsgi.py to enable bootstrapping django app before import

--- src/sensor/wsgi.py
+++ src/sensor/wsgi.py
@@ -4,6 +4,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+
+isort:skip_file
+
 """
 
 import os
@@ -11,12 +14,11 @@ import os
 import django
 from django.core.wsgi import get_wsgi_application
 
-from scheduler import scheduler  # noqa
-from sensor import settings  # noqa
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensor.settings")
 django.setup()  # this is necessary because we need to handle our own thread
 
+from scheduler import scheduler  # noqa
+from sensor import settings  # noqa
 
 application = get_wsgi_application()
 
