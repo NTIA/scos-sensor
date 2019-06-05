404579419a8ff916c6af5db741b8b5ce8c1969e1
Douglas Anderson <douglas.j.anderson@gmail.com>
2019-06-04 23:43:12 -0600

    Fix task overview test - ensure "archive" is present only

--- src/tasks/tests/test_overview_view.py
+++ src/tasks/tests/test_overview_view.py
@@ -24,7 +24,7 @@ def test_user_get_overview(user_client):
     create_task_results(2, user_client)
     overview, = get_results_overview(user_client)
     assert overview["task_results_available"] == 2
-    assert overview["archive"]  # is non-empty string
+    assert overview["archive"] is None  # indicates no acquisition data available
     assert overview["task_results"]  # is non-empty string
     assert overview["schedule_entry"]  # is non-empty string
 
@@ -33,7 +33,7 @@ def test_admin_get_overview(admin_client):
     create_task_results(2, admin_client)
     overview, = get_results_overview(admin_client)
     assert overview["task_results_available"] == 2
-    assert overview["archive"]  # is non-empty string
+    assert overview["archive"] is None  # indicates no acquisition data available
     assert overview["task_results"]  # is non-empty string
     assert overview["schedule_entry"]  # is non-empty string
 
