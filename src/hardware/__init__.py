41939962830ff57347436cfd313b51a33cfe35f5
Justin Haze <jhaze@ntia.doc.gov>
2020-03-02 11:15:08 -0700

    cleanup radio initialization

--- src/hardware/__init__.py
+++ src/hardware/__init__.py
@@ -1,24 +1,17 @@
 from sensor import settings
 from hardware.usrp_radio import USRPRadio
 
-usrp_radio = None
-keysight_n6841a_radio = None
 radio = None
 
 
 def get_radio():
-    global usrp_radio
+    global radio
 
     if settings.SENSOR_TYPE == "USRP":
-        if not usrp_radio:
-            usrp_radio = USRPRadio()
-        return usrp_radio
-    # elif settings.SENSOR_TYPE == "KEYSIGHT_N6841A":
-    #     if not keysight_n6841a_radio:
-    #         keysight_n6841a_radio = KeysightN6841ARadio()
-    #     return keysight_n6841a_radio
+        if not radio:
+            radio = USRPRadio()
     else:
         raise Exception("Unsupported SENSOR_TYPE")
 
 
-radio = get_radio()
+get_radio()
