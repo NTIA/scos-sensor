8d9e500583aaa37fe98042fad510fcc3f55d3b33
Justin Haze <jhaze@ntia.doc.gov>
2020-04-08 13:31:35 -0600

    remove install_drivers

--- entrypoints/api_entrypoint.sh
+++ entrypoints/api_entrypoint.sh
@@ -11,9 +11,6 @@ function cleanup_demodb {
 trap cleanup_demodb SIGTERM
 trap cleanup_demodb SIGINT
 
-echo "Installing Drivers"
-python3 /scripts/install_drivers.py
-
 echo "Starting Migrations"
 python3 manage.py migrate
 
