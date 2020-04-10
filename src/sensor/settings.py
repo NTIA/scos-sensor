c372082c64308c86fbe225e00dfe7cb8e449cb37
Justin Haze <jhaze@ntia.doc.gov>
2020-04-10 09:42:13 -0600

    restore calibration file settings

--- src/sensor/settings.py
+++ src/sensor/settings.py
@@ -61,6 +61,8 @@ OPENAPI_FILE = path.join(REPO_ROOT, "docs", "openapi.json")
 CONFIG_DIR = path.join(REPO_ROOT, "configs")
 
 # JSON configs
+SENSOR_CALIBRATION_FILE = path.join(CONFIG_DIR, "sensor_calibration.json")
+SIGAN_CALIBRATION_FILE = path.join(CONFIG_DIR, "sigan_calibration.json")
 SENSOR_DEFINITION_FILE = path.join(CONFIG_DIR, "sensor_definition.json")
 MEDIA_ROOT = path.join(REPO_ROOT, "files")
 
