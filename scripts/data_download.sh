1316a28340b1dddbb25a6e91a4dc531bc20280d2
Brad Eales <bradeales@users.noreply.github.com>
2020-04-16 12:48:08 -0600

    Takes config files specified on the comnmad line
    Adds the ability to specify config files on the command line. If none are specified, prompt user for input.

--- scripts/data_download.sh
+++ scripts/data_download.sh
@@ -2,39 +2,46 @@
 
 # A quick script to download data directly from a scos-sensor
 
-# Read in settings from last run (if present)
-source ./data_download.cfg
-
-doublecheck="n"
+# Set variables
+doublecheck="y"
 remaining=0
 
-read -e -i "$ip" -p "Enter the IP of the SCOS sensor: " ip
-read -e -i "$token" -p "Enter the auth token: " token # Obtain token from "user" endpoint on the browseable api
-read -e -i "$schedule" -p "Enter the schedule name you want to download from: " schedule # Obtain schedule name from "schedule" endpoint on the browseable api
-read -e -i "$firstfile" -p "Enter the first file number you want to copy: " firstfile
-read -e -i "$lastfile" -p "Enter the last number file you want to copy: " lastfile # See # files from the "acquisitions" endpoint on the browasble api
-read -e -i "$filepath" -p "Where should these files be saved?: " filepath
-read -e -i "$cleanup" -p "Enter minimum filesize to keep (Megabytes): " cleanup 
+# Read in config file from command line (if present)
+source "$1"
 
-printf "\n### CONFIG SUMMARY ###\n"
-printf "Sensor IP: $ip\n"
-printf "Auth token: $token\n"
-printf "Schedule name: $schedule\n"
-printf "First file to copy: $firstfile\n"
-printf "Last file to copy: $lastfile\n"
-printf "Save location: $filepath\n"
-printf "Minimum filesize: ${cleanup}MB \n"
+# Take user input if no config file is given, and save the config
+if [ "$1" == "" ]; then
+    set -- data_download.cfg
+    doublecheck="n"
 
-read -e -i "$doublecheck" -p "Check the above settings. Do you wish to proceed (y/n)? " doublecheck
+    read -e -i "$ip" -p "Enter the IP of the SCOS sensor: " ip
+    read -e -i "$token" -p "Enter the auth token: " token # Obtain token from "user" endpoint on the browseable api
+    read -e -i "$schedule" -p "Enter the schedule name you want to download from: " schedule # Obtain schedule name from "schedule" endpoint on the browseable api
+    read -e -i "$firstfile" -p "Enter the first file number you want to copy: " firstfile
+    read -e -i "$lastfile" -p "Enter the last number file you want to copy: " lastfile # See # files from the "acquisitions" endpoint on the browasble api
+    read -e -i "$filepath" -p "Where should these files be saved?: " filepath
+    read -e -i "$cleanup" -p "Enter minimum filesize to keep (Megabytes): " cleanup
 
-# Save settings as defaults for next time
-echo "ip=$ip" > ./data_download.cfg
-echo "token=$token" >> ./data_download.cfg
-echo "schedule=$schedule" >> ./data_download.cfg
-echo "firstfile=$firstfile" >> ./data_download.cfg
-echo "lastfile=$lastfile" >> ./data_download.cfg
-echo "filepath=$filepath" >> ./data_download.cfg
-echo "cleanup=$cleanup" >> ./data_download.cfg
+    printf "\n### CONFIG SUMMARY ###\n"
+    printf "Sensor IP: $ip\n"
+    printf "Auth token: $token\n"
+    printf "Schedule name: $schedule\n"
+    printf "First file to copy: $firstfile\n"
+    printf "Last file to copy: $lastfile\n"
+    printf "Save location: $filepath\n"
+    printf "Minimum filesize: ${cleanup}MB \n"
+
+    read -e -i "$doublecheck" -p "Check the above settings. Do you wish to proceed (y/n)? " doublecheck
+
+    # Save settings as defaults for next time
+    echo "ip=$ip" > ./$1
+    echo "token=$token" >> ./$1
+    echo "schedule=$schedule" >> ./$1
+    echo "firstfile=$firstfile" >> ./$1
+    echo "lastfile=$lastfile" >> ./$1
+    echo "filepath=$filepath" >> ./$1
+    echo "cleanup=$cleanup" >> ./$1
+fi
 
 # Delete files that are under size
 find $filepath -name "*.sigmf" -type 'f' -size -${cleanup}M -delete
@@ -46,9 +53,9 @@ if [ $doublecheck == "y" ]; then
         start=$SECONDS
         if [ ! -f $filepath/$schedule\_$i.sigmf ]; then
             curl -o $filepath/$schedule\_$i.sigmf -kLsS -H "Authorization: Token $token" https://$ip/api/v1/tasks/completed/$schedule/$i/archive
+            remaining=$(( (((SECONDS - start) * (lastfile - firstfile - i) / 60) + remaining) / 2 ))
+            printf "Downloaded ${schedule}_${i}.sigmf. ${remaining} mins remaining. \n"
         fi
-        remaining=$(( (((SECONDS - start) * (lastfile - firstfile - i) / 60) + remaining) / 2 ))
-        printf "Downloaded ${schedule}_${i}.sigmf. ${remaining} mins remaining. \n"
     done
     printf "Copy finished: `date` \n"
     printf "\n### COPY COMPLETED ###\n"
