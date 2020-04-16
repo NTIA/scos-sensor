c18e6eb907fe4ec86c70988fd2715d024ff9b60a
Brad Eales <bradeales@users.noreply.github.com>
2020-04-15 22:23:58 -0600

    Works with new API
    Works with latest API. Added functionality to delete previously downloaded but undersized files from the save location (either due to timeout or transfer corruption), and then proceed to copy files that do not exist in the save location.

--- scripts/data_download.sh
+++ scripts/data_download.sh
@@ -14,6 +14,7 @@ read -e -i "$schedule" -p "Enter the schedule name you want to download from: " 
 read -e -i "$firstfile" -p "Enter the first file number you want to copy: " firstfile
 read -e -i "$lastfile" -p "Enter the last number file you want to copy: " lastfile # See # files from the "acquisitions" endpoint on the browasble api
 read -e -i "$filepath" -p "Where should these files be saved?: " filepath
+read -e -i "$cleanup" -p "Enter minimum filesize to keep (Megabytes): " cleanup 
 
 printf "\n### CONFIG SUMMARY ###\n"
 printf "Sensor IP: $ip\n"
@@ -22,6 +23,7 @@ printf "Schedule name: $schedule\n"
 printf "First file to copy: $firstfile\n"
 printf "Last file to copy: $lastfile\n"
 printf "Save location: $filepath\n"
+printf "Minimum filesize: ${cleanup}MB \n"
 
 read -e -i "$doublecheck" -p "Check the above settings. Do you wish to proceed (y/n)? " doublecheck
 
@@ -32,13 +34,19 @@ echo "schedule=$schedule" >> ./data_download.cfg
 echo "firstfile=$firstfile" >> ./data_download.cfg
 echo "lastfile=$lastfile" >> ./data_download.cfg
 echo "filepath=$filepath" >> ./data_download.cfg
+echo "cleanup=$cleanup" >> ./data_download.cfg
+
+# Delete files that are under size
+find $filepath -name "*.sigmf" -type 'f' -size -${cleanup}M -delete
 
 # Copy files using curl
 if [ $doublecheck == "y" ]; then
     printf "Copy started: `date` \n"
     for i in $(seq $firstfile $lastfile); do
         start=$SECONDS
-        curl -o $filepath/$schedule\_$i.sigmf -kLsS -H "Authorization: Token $token" https://$ip/api/v1/acquisitions/$schedule/$i/archive
+        if [ ! -f $filepath/$schedule\_$i.sigmf ]; then
+            curl -o $filepath/$schedule\_$i.sigmf -kLsS -H "Authorization: Token $token" https://$ip/api/v1/tasks/completed/$schedule/$i/archive
+        fi
         remaining=$(( (((SECONDS - start) * (lastfile - firstfile - i) / 60) + remaining) / 2 ))
         printf "Downloaded ${schedule}_${i}.sigmf. ${remaining} mins remaining. \n"
     done
