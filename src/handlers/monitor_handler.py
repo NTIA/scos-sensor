16f0cd153a1bd4557200a3dbfc7bf491bac0d854
Justin Haze <jhaze@ntia.doc.gov>
2020-08-04 12:36:35 -0600

    add monitor_action_completed_callback

---
+++ src/handlers/monitor_handler.py
@@ -0,0 +1,12 @@
+from sensor import settings
+from pathlib import Path
+
+def monitor_action_completed_callback(sender, **kwargs):
+    healthy = kwargs["radio_healthy"]
+    if healthy:
+        Path(settings.SDR_HEALTHCHECK_FILE).unlink()
+        logger.info("USRP healthy")
+    else:
+        logger.warning("USRP unhealthy")
+        if settings.IN_DOCKER:
+            Path(settings.SDR_HEALTHCHECK_FILE).touch()
