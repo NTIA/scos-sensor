a03763696e90a189a4787bcd8fef709e6508da22
Justin Haze <jhaze@ntia.doc.gov>
2020-08-05 08:42:36 -0600

    check if SDR healthcheck file exists before delete

--- src/handlers/monitor_handler.py
+++ src/handlers/monitor_handler.py
@@ -1,13 +1,15 @@
 from sensor import settings
 from pathlib import Path
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 def monitor_action_completed_callback(sender, **kwargs):
     healthy = kwargs["radio_healthy"]
     if healthy:
-        Path(settings.SDR_HEALTHCHECK_FILE).unlink()
+        if os.path.exists(settings.SDR_HEALTHCHECK_FILE):
+            Path(settings.SDR_HEALTHCHECK_FILE).unlink()
         logger.info("USRP healthy")
     else:
         logger.warning("USRP unhealthy")
