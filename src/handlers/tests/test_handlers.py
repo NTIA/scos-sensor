58930e2a9d41a3c8f5d643c25a71c87d47955c87
dboulware <dboulware@ntia.doc.gov>
2022-03-17 15:07:05 -0600

    Adds test to ensure deletion of inactive locations doesn't update capabilities in memory.

--- src/handlers/tests/test_handlers.py
+++ src/handlers/tests/test_handlers.py
@@ -53,3 +53,25 @@ def test_db_location_deleted_handler():
     assert capabilities['sensor']['location']['description'] == 'test'
     location.delete()
     assert capabilities['sensor']['location'] is None
+
+@pytest.mark.django_db
+def test_db_location_deleted_inactive_handler():
+    capabilities['sensor'] ={}
+    capabilities['sensor']['location'] = {}
+    location = Location()
+    location.gps = False
+    location.height=10
+    location.longitude = 100
+    location.latitude = -1
+    location.description = 'test'
+    location.active = False
+    location.save()
+    assert capabilities['sensor']['location']['x'] == 100
+    assert capabilities['sensor']['location']['y'] == -1
+    assert capabilities['sensor']['location']['z'] == 10
+    assert capabilities['sensor']['location']['description'] == 'test'
+    location.delete()
+    assert capabilities['sensor']['location']['x'] == 100
+    assert capabilities['sensor']['location']['y'] == -1
+    assert capabilities['sensor']['location']['z'] == 10
+    assert capabilities['sensor']['location']['description'] == 'test'
