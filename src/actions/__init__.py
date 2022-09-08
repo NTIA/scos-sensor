7d1363a30ba30a22395becea62e294b58875ab42
dboulware <dboulware@ntia.doc.gov>
2022-09-08 17:06:41 -0600

    Removed registered_actions to remove the temptation to do bad things. Updated versions of scos-actions and scos-tekrsa.

--- src/actions/__init__.py
+++ src/actions/__init__.py
@@ -8,6 +8,7 @@ from scos_actions.discover import test_actions
 from sensor import settings
 from sensor.utils import copy_driver_files
 
+#
 logger = logging.getLogger(__name__)
 logger.debug("************ Initializing scos-sensor/actions ************ ")
 
@@ -20,13 +21,9 @@ discovered_plugins = {
 }
 logger.debug(discovered_plugins)
 
-# Actions initialized here are made available through the API
-registered_actions = {}
-
 if settings.MOCK_SIGAN or settings.RUNNING_TESTS:
     for name, action in test_actions.items():
         logger.debug("test_action: " + name + "=" + str(action))
-        registered_actions[name] = action
         register_action.send(sender=__name__, action=action)
 else:
     for name, module in discovered_plugins.items():
@@ -34,6 +31,5 @@ else:
         discover = importlib.import_module(name + ".discover")
         for name, action in discover.actions.items():
             logger.debug("action: " + name + "=" + str(action))
-            registered_actions[name] = action
             register_action.send(sender=__name__, action=action)
 logger.debug("Finished loading actions")
