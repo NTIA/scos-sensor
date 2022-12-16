bba6e24a21b3007af4e25509d7190c6f05e40a2f
Justin Haze <jhaze@ntia.doc.gov>
2022-12-16 16:03:54 -0700

    change monitor handler to health check handler, update scos-actions and scos-usrp branches, format files

--- src/handlers/apps.py
+++ src/handlers/apps.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_delete, post_save
 from scos_actions.signals import (
     location_action_completed,
     measurement_action_completed,
-    monitor_action_completed,
+    trigger_api_restart,
 )
 
 logger = logging.getLogger(__name__)
@@ -15,13 +15,13 @@ class HandlersConfig(AppConfig):
     name = "handlers"
 
     def ready(self):
+        from handlers.health_check_handler import trigger_api_restart_callback
         from handlers.location_handler import (
             db_location_deleted,
             db_location_updated,
             location_action_completed_callback,
         )
         from handlers.measurement_handler import measurement_action_completed_callback
-        from handlers.monitor_handler import monitor_action_completed_callback
 
         measurement_action_completed.connect(measurement_action_completed_callback)
         logger.debug(
@@ -37,7 +37,7 @@ class HandlersConfig(AppConfig):
         post_delete.connect(db_location_deleted)
         logger.debug("db_location_deleted registered to post_delete")
 
-        monitor_action_completed.connect(monitor_action_completed_callback)
+        trigger_api_restart.connect(trigger_api_restart_callback)
         logger.debug(
             "monitor_action_completed_callback registered to monitor_action_completed"
         )
