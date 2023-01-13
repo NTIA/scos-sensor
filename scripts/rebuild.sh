b7aac5eef24e809ef7ca12d371bf7d930189addc
Anthony Romaniello <aromaniello@ntia.gov>
2023-01-13 14:01:25 -0700

    remove rebuild script

--- scripts/rebuild.sh
+++
@@ -1,6 +0,0 @@
-docker-compose down
-sudo rm -rf /home/deploy/scos-sensor/dbdata
-docker system prune -af
-docker-compose build --no-cache
-docker-compose up -d --force-recreate
-docker-compose logs -f
