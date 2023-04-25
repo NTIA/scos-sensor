83d9bb3e531ee8e34e4357a1119d954b0e1a704b
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-25 10:57:44 -0400

    add unit tests for sensor.utils datetime manipulations

--- src/sensor/utils.py
+++ src/sensor/utils.py
@@ -9,16 +9,16 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
-def get_datetime_from_timestamp(ts):
+def get_datetime_from_timestamp(ts: int) -> datetime:
     return datetime.fromtimestamp(ts)
 
 
-def get_timestamp_from_datetime(dt: datetime):
+def get_timestamp_from_datetime(dt: datetime) -> int:
     """Assumes UTC datetime."""
     return int(dt.timestamp())
 
 
-def parse_datetime_str(d):
+def parse_datetime_str(d: str) -> datetime:
     return datetime.strptime(d, settings.DATETIME_FORMAT)
 
 
