231f9950f2062027fce0c0f59229c99448de7e4e
Anthony Romaniello <aromaniello@ntia.gov>
2023-05-08 21:03:31 -0400

    Use django-rest-knox for token authentication
    Commit does not include edits to Django settings

--- src/authentication/auth.py
+++ src/authentication/auth.py
@@ -10,7 +10,7 @@ from rest_framework.authentication import get_authorization_header
 logger = logging.getLogger(__name__)
 
 token_auth_enabled = (
-    "rest_framework.authentication.TokenAuthentication"
+    "knox.auth.TokenAuthentication"
     in settings.REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"]
 )
 oauth_jwt_authentication_enabled = (
