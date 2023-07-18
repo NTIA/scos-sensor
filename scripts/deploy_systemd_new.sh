81447a3ea8e9e92a614147c9667f9fda43c700b4
Justin Haze <jhaze@ntia.gov>
2023-07-18 15:25:31 -0600

    script to generate systemd with `--new` flag

---
+++ scripts/deploy_systemd_new.sh
@@ -0,0 +1,20 @@
+cd $REPO_ROOT
+systemctl --user stop pod_scos-sensor.service
+systemctl --user disable pod_scos-sensor.service
+rm -f ~/.config/systemd/user/pod_scos-sensor.service
+rm -f ~/.config/systemd/user/container-scos-sensor_api_1.service
+rm -f ~/.config/systemd/user/container-scos-sensor_db_1.service
+rm -f ~/.config/systemd/user/container-scos-sensor_nginx_1.service
+rm -f ~/.config/systemd/user/default.target.wants/pod_scos-sensor.service
+systemctl --user daemon-reload
+systemctl --user reset-failed
+rm -f *.service
+podman pod rm pod_scos-sensor
+sudo rm -rf dbdata
+podman-compose --in-pod scos_pod --pod-args="--infra=true" --podman-run-args="--health-on-failure=restart" up --build --no-start
+podman generate systemd --new --files --name --pod-prefix "" pod_scos-sensor # may need to clear out old service files first
+mkdir -p ~/.config/systemd/user
+cp *.service ~/.config/systemd/user
+systemctl --user enable pod_scos-sensor.service
+systemctl --user daemon-reload
+systemctl --user start pod_scos-sensor.service
