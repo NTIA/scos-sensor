4e2f9f440cbd377243eeef616bcee9d7b76d2a96
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-10 12:03:02 -0700

    pass gps to actions in configs/actions.

--- src/actions/__init__.py
+++ src/actions/__init__.py
@@ -2,18 +2,12 @@ import importlib
 import logging
 import pkgutil
 
-from scos_actions.discover import test_actions
+from scos_actions.discover import init, test_actions
 from scos_actions.signals import register_action
-from scos_actions.discover import init
-
 
 from sensor import settings
 from sensor.utils import copy_driver_files
-
-from utils.component_registrar import (
-    sigan_monitor,
-    gps_monitor
-)
+from utils.component_registrar import gps_monitor, sigan_monitor
 
 logger = logging.getLogger(__name__)
 logger.debug("********** Initializing actions **********")
@@ -40,9 +34,12 @@ else:
             register_action.send(sender=__name__, action=action)
 
 logger.debug(f"Loading actions in {settings.ACTIONS_DIR}")
-yaml_actions, yaml_test_actions = init(sigan=sigan_monitor.signal_analyzer, yaml_dir=settings.ACTIONS_DIR)
+yaml_actions, yaml_test_actions = init(
+    sigan=sigan_monitor.signal_analyzer,
+    gps=gps_monitor.gps,
+    yaml_dir=settings.ACTIONS_DIR,
+)
 for name, action in yaml_actions.items():
     logger.debug("action: " + name + "=" + str(action))
     register_action.send(sender=__name__, action=action)
 logger.debug("Finished loading actions")
-
