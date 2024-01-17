63e8f43f82ff8707db1d3cf2bc158f2693f68fb4
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-17 07:30:29 -0700

    Fix tests.

--- src/initialization/tests/test_initialization.py
+++ src/initialization/tests/test_initialization.py
@@ -1,9 +1,15 @@
 from src.initialization import load_preselector
+import logging
+import os
 
+logger = logging.getLogger(__name__)
 
 def test_load_preselector():
-    preselector = load_preselector(preselector_config=
-        {"name": "test", "base_url": "http://127.0.0.1"},
+    preselector_config = os.getcwd()
+    index = preselector_config.index("src")
+    preselector_config = os.path.join( preselector_config[:index], "configs/preselector_config.json")
+    logger.debug("Loading preselector config: " + preselector_config)
+    preselector = load_preselector(preselector_config=preselector_config,
         module="its_preselector.web_relay_preselector",
         preselector_class_name = "WebRelayPreselector", sensor_definition={}
     )
