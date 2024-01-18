9dc682f9278916a4383f3537b3c7dd53caef48b9
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-18 07:50:37 -0700

    Move location updating in status to app ready.

--- src/status/__init__.py
+++ src/status/__init__.py
@@ -1,20 +1,7 @@
 import logging
-from .models import Location
 from initialization import sensor_loader
-from scos_actions.metadata.utils import construct_geojson_point
 
 logger = logging.getLogger(__name__)
 logger.debug("********** Initializing status **********")
-start_time = sensor_loader.sensor.start_time
-try:
-    location = Location.objects.get(active=True)
-    db_location_geojson = construct_geojson_point(
-            location.longitude,
-            location.latitude,
-            location.height
-    )
-    logger.debug(f"Location found in DB. Updating sensor location to {location}.")
-    if sensor_loader.sensor is not None:
-        sensor_loader.sensor.location = db_location_geojson
-except Location.DoesNotExist:
-    pass
+if sensor_loader.sensor is not None:
+    start_time = sensor_loader.sensor.start_time
--- src/status/apps.py
+++ src/status/apps.py
@@ -1,5 +1,25 @@
+import logging
 from django.apps import AppConfig
+from initialization import sensor_loader
+from .models import Location
+from scos_actions.metadata.utils import construct_geojson_point
 
+
+logger = logging.getLogger(__name__)
 
 class StatusConfig(AppConfig):
     name = "status"
+
+    def ready(self):
+        try:
+            location = Location.objects.get(active=True)
+            db_location_geojson = construct_geojson_point(
+                location.longitude,
+                location.latitude,
+                location.height
+            )
+            logger.debug(f"Location found in DB. Updating sensor location to {location}.")
+            if sensor_loader.sensor is not None:
+                sensor_loader.sensor.location = db_location_geojson
+        except Location.DoesNotExist:
+            pass
