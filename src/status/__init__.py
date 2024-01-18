6d2382fe05261fe061fe1eed377a4d00762c7fc4
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-18 07:32:21 -0700

    Update sensor location with database location on startup.

--- src/status/__init__.py
+++ src/status/__init__.py
@@ -1,10 +1,23 @@
 import datetime
 import logging
+from .models import Location
 from initialization import sensor_loader
+from scos_actions.metadata.utils import construct_geojson_point
 
 logger = logging.getLogger(__name__)
 logger.debug("********** Initializing status **********")
 start_time = sensor_loader.sensor.start_time
+try:
+    location = Location.objects.get(active=True)
+    db_location_geojson = construct_geojson_point(
+            location.longitude,
+            location.latitude,
+            location.height,
+    )
+    logger.debug(f"Location found in DB. Updating sensor location to {location}.")
+    sensor_loader.sensor.location = db_location_geojson
+except Location.DoesNotExist:
+    #No location, no problem
 
 
 
