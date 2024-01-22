ddbe4d0ad30b161248fca9bd6d3e49f1a2f6a4ee
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-22 08:18:11 -0700

    Set start_time in status when sigan is not initialized.

--- src/status/__init__.py
+++ src/status/__init__.py
@@ -1,7 +1,10 @@
+import datetime
 import logging
 from initialization import sensor_loader
 
 logger = logging.getLogger(__name__)
 logger.debug("********** Initializing status **********")
-if sensor_loader.sensor is not None:
+if sensor_loader.sigan is not None:
     start_time = sensor_loader.sensor.start_time
+else:
+    start_time =  datetime.datetime.utcnow()
