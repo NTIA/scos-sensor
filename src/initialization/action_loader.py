6b6882a8155252e9e8ef241f6070348ee9461695
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-22 12:42:41 -0700

    correct Action import.

--- src/initialization/action_loader.py
+++ src/initialization/action_loader.py
@@ -9,7 +9,7 @@ from typing import Dict
 from django.conf import settings
 from scos_actions.actions import action_classes
 from scos_actions.discover import init, test_actions
-from scos_actions.interfaces.action import Action
+from scos_actions.actions.interfaces.action import Action
 
 logger = logging.getLogger(__name__)
 
