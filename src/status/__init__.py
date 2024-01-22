657fdae95f8fc3145668f3517688bce76ae3a53d
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-22 08:49:07 -0700

    correct sigan -> signal_analyzer.

--- src/status/__init__.py
+++ src/status/__init__.py
@@ -4,7 +4,7 @@ from initialization import sensor_loader
 
 logger = logging.getLogger(__name__)
 logger.debug("********** Initializing status **********")
-if sensor_loader.sigan is not None:
+if sensor_loader.signal_analyzer is not None:
     start_time = sensor_loader.sensor.start_time
 else:
     start_time =  datetime.datetime.utcnow()
