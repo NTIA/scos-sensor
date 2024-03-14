d0c24cc7d78b7c4e444e6333823a8eace7e93951
Doug Boulware <dboulware@ntia.doc.gov>
2024-03-14 15:51:41 -0600

    Merge in autohealing fixes. Move cal on startup check to initialization/__init__.py to ensure healthy signal analyzer.

--- src/initialization/sensor_loader.py
+++ src/initialization/sensor_loader.py
@@ -1,21 +1,14 @@
 import importlib
 import logging
-from os import path
-from pathlib import Path
-from typing import Optional, Union
 
 from django.conf import settings
 from environs import Env
-from its_preselector.configuration_exception import ConfigurationException
-from its_preselector.controlbyweb_web_relay import ControlByWebWebRelay
 from its_preselector.preselector import Preselector
-from scos_actions import utils
-from scos_actions.calibration.differential_calibration import DifferentialCalibration
-from scos_actions.calibration.sensor_calibration import SensorCalibration
 from scos_actions.hardware.sensor import Sensor
 from scos_actions.metadata.utils import construct_geojson_point
-
 from utils.signals import register_component_with_status
+
+from .utils import get_usb_device_exists, set_container_unhealthy
 
 logger = logging.getLogger(__name__)
 env = Env()
@@ -24,14 +17,16 @@ env = Env()
 class SensorLoader:
     _instance = None
 
-    def __init__(self, sensor_capabilities: dict, sensor_actions: dict):
+    def __init__(
+            self, sensor_capabilities: dict, switches: dict, preselector: Preselector
+    ):
         if not hasattr(self, "sensor"):
             logger.debug("Sensor has not been loaded. Loading...")
-            self._sensor = load_sensor(sensor_capabilities, sensor_actions)
+            self._sensor = load_sensor(sensor_capabilities,switches, preselector)
         else:
             logger.debug("Already loaded sensor. ")
 
-    def __new__(cls, sensor_capabilities: dict, sensor_actions: dict):
+    def __new__(cls, sensor_capabilities: dict,switches: dict, preselector: Preselector):
         if cls._instance is None:
             logger.debug("Creating the SensorLoader")
             cls._instance = super().__new__(cls)
@@ -42,8 +37,7 @@ class SensorLoader:
         return self._sensor
 
 
-def load_sensor(sensor_capabilities: dict, sensor_actions: dict) -> Sensor:
-    switches = {}
+def load_sensor(sensor_capabilities: dict, switches: dict, preselector: Preselector, sensor_actions: dict) -> Sensor:
     sensor_cal = None
     differential_cal = None
     preselector = None
@@ -59,28 +53,25 @@ def load_sensor(sensor_capabilities: dict, sensor_actions: dict) -> Sensor:
                 sensor_loc["y"],
                 sensor_loc["z"] if "z" in sensor_loc else None,
             )
-        switches = load_switches(settings.SWITCH_CONFIGS_DIR)
-        preselector = load_preselector(
-            settings.PRESELECTOR_CONFIG,
-            settings.PRESELECTOR_MODULE,
-            settings.PRESELECTOR_CLASS,
-            sensor_capabilities["sensor"],
-        )
 
     sigan = None
     try:
         if not settings.RUNNING_MIGRATIONS:
-            check_for_required_sigan_settings()
-            sigan_module_setting = settings.SIGAN_MODULE
-            sigan_module = importlib.import_module(sigan_module_setting)
-            logger.info(f"Creating {settings.SIGAN_CLASS} from {settings.SIGAN_MODULE}")
-            sigan_constructor = getattr(sigan_module, settings.SIGAN_CLASS)
-            sigan = sigan_constructor(switches=switches)
-            register_component_with_status.send(sigan, component=sigan)
+            if get_usb_device_exists():
+                check_for_required_sigan_settings()
+                sigan_module_setting = settings.SIGAN_MODULE
+                sigan_module = importlib.import_module(sigan_module_setting)
+                logger.info(f"Creating {settings.SIGAN_CLASS} from {settings.SIGAN_MODULE}")
+                sigan_constructor = getattr(sigan_module, settings.SIGAN_CLASS)
+                sigan = sigan_constructor(switches=switches)
+                register_component_with_status.send(sigan, component=sigan)
+            else:
+                logger.warning("Required USB Device does not exist.")
         else:
             logger.info("Running migrations. Not loading signal analyzer.")
-    except Exception as ex:
+    except BaseException as ex:
         logger.warning(f"unable to create signal analyzer: {ex}")
+        set_container_unhealthy()
 
     # Create sensor before handling calibrations
     sensor = Sensor(
@@ -93,42 +84,6 @@ def load_sensor(sensor_capabilities: dict, sensor_actions: dict) -> Sensor:
         sensor_cal=None,
         differential_cal=None,
     )
-
-    # Calibration loading
-    if not settings.RUNNING_TESTS:
-        # Load the onboard cal file as the sensor calibration, if it exists
-        onboard_cal = get_calibration(settings.ONBOARD_CALIBRATION_FILE, "onboard")
-        if onboard_cal is not None:
-            sensor.sensor_calibration = onboard_cal
-        else:
-            # Otherwise, try using the sensor calibration file
-            sensor_cal = get_calibration(settings.SENSOR_CALIBRATION_FILE, "sensor")
-            if sensor_cal is not None:
-                sensor.sensor_calibration = sensor_cal
-
-        # Now run the calibration action defined in the environment
-        # This will create an onboard_cal file if needed, and set it
-        # as the sensor's sensor_calibration.
-        if not settings.RUNNING_MIGRATIONS:
-            if sensor.sensor_calibration is None or sensor.sensor_calibration.expired():
-                if settings.STARTUP_CALIBRATION_ACTION is None:
-                    logger.error("No STARTUP_CALIBRATION_ACTION set.")
-                else:
-                    cal_action = sensor_actions[settings.STARTUP_CALIBRATION_ACTION]
-                    cal_action(sensor=sensor, schedule_entry=None, task_id=None)
-            else:
-                logger.debug(
-                    "Skipping startup calibration since sensor_calibration exists and"
-                    + "CALIBRATE_ON_STARTUP environment variable is False"
-                )
-
-        # Now load the differential calibration, if it exists
-        differential_cal = get_calibration(
-            settings.DIFFERENTIAL_CALIBRATION_FILE,
-            "differential",
-        )
-        sensor.differential_calibration = differential_cal
-
     return sensor
 
 
@@ -145,99 +100,3 @@ def check_for_required_sigan_settings():
         raise Exception(error)
 
 
-def load_switches(switch_dir: Path) -> dict:
-    logger.debug(f"Loading switches in {switch_dir}")
-    switch_dict = {}
-    try:
-        if switch_dir is not None and switch_dir.is_dir():
-            for f in switch_dir.iterdir():
-                file_path = f.resolve()
-                logger.debug(f"loading switch config {file_path}")
-                conf = utils.load_from_json(file_path)
-                try:
-                    switch = ControlByWebWebRelay(conf)
-                    logger.debug(f"Adding {switch.id}")
-                    switch_dict[switch.id] = switch
-                    logger.debug(f"Registering switch status for {switch.name}")
-                    register_component_with_status.send(__name__, component=switch)
-                except ConfigurationException:
-                    logger.error(f"Unable to configure switch defined in: {file_path}")
-    except Exception as ex:
-        logger.error(f"Unable to load switches {ex}")
-    return switch_dict
-
-
-def load_preselector_from_file(
-    preselector_module, preselector_class, preselector_config_file: Path
-):
-    if preselector_config_file is None:
-        return None
-    else:
-        try:
-            preselector_config = utils.load_from_json(preselector_config_file)
-            return load_preselector(
-                preselector_config, preselector_module, preselector_class
-            )
-        except ConfigurationException:
-            logger.exception(
-                f"Unable to create preselector defined in: {preselector_config_file}"
-            )
-    return None
-
-
-def load_preselector(
-    preselector_config: str,
-    module: str,
-    preselector_class_name: str,
-    sensor_definition: dict,
-) -> Preselector:
-    logger.debug(
-        f"loading {preselector_class_name} from {module} with config: {preselector_config}"
-    )
-    if module is not None and preselector_class_name is not None:
-        preselector_module = importlib.import_module(module)
-        preselector_constructor = getattr(preselector_module, preselector_class_name)
-        preselector_config = utils.load_from_json(preselector_config)
-        ps = preselector_constructor(sensor_definition, preselector_config)
-        register_component_with_status.send(ps, component=ps)
-    else:
-        ps = None
-    return ps
-
-
-def get_calibration(
-    cal_file_path: str, cal_type: str
-) -> Optional[Union[DifferentialCalibration, SensorCalibration]]:
-    """
-    Load calibration data from file.
-
-    :param cal_file_path: Path to the JSON calibration file.
-    :param cal_type: Calibration type to load: "onboard", "sensor" or "differential"
-    :return: The ``Calibration`` object, if loaded, or ``None`` if loading failed.
-    """
-    try:
-        cal = None
-        if cal_file_path is None or cal_file_path == "":
-            logger.error("No calibration file specified.")
-            raise ValueError
-        elif not path.exists(cal_file_path):
-            logger.error(f"{cal_file_path} does not exist.")
-            raise FileNotFoundError
-        else:
-            logger.debug(f"Loading calibration file: {cal_file_path}")
-            # Create calibration object
-            cal_file_path = Path(cal_file_path)
-            if cal_type.lower() in ["sensor", "onboard"]:
-                cal = SensorCalibration.from_json(cal_file_path)
-            elif cal_type.lower() == "differential":
-                cal = DifferentialCalibration.from_json(cal_file_path)
-            else:
-                logger.error(f"Unknown calibration type: {cal_type}")
-                raise ValueError
-    except Exception:
-        cal = None
-        logger.exception(
-            f"Unable to load {cal_type} calibration file, reverting to none"
-        )
-    finally:
-        return cal
---
+++ src/initialization/utils.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+from pathlib import Path
+from subprocess import check_output
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+def set_container_unhealthy():
+    if settings.IN_DOCKER:
+        logger.warning("Marking container for restart.")
+        Path(settings.SDR_HEALTHCHECK_FILE).touch()
+
+
+def get_usb_device_exists() -> bool:
+    logger.debug("Checking for USB...")
+    if not settings.RUNNING_TESTS and settings.USB_DEVICE is not None:
+        usb_devices = check_output("lsusb").decode(sys.stdout.encoding)
+        logger.debug("Checking for " + settings.USB_DEVICE)
+        logger.debug("Found " + usb_devices)
+        return settings.USB_DEVICE in usb_devices
+    return True
